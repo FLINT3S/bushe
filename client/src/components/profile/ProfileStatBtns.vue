a41307b5a67a5ec67709aefc8ae6b61f0e1f7b56
Ilya <sia.ilya.dev@gmail.com>
2023-06-24 20:32:24 +0300

    fix: Фикс сборки

--- client/src/components/profile/ProfileStatBtns.vue
+++ client/src/components/profile/ProfileStatBtns.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="row row-cols-2">
-    <div class="col">
-      <n-card size="large">
-            Доставлено в этом месяце: {{stats.deliveriedMonth}}
-      </n-card>
+    <div class="row row-cols-2">
+        <div class="col">
+            <n-card size="large">
+                Доставлено в этом месяце: {{ stats.deliveriedMonth }}
+            </n-card>
+        </div>
+        <div class="col">
+            <n-card size="large">
+                Заработано в этом месяце: {{ stats.earnedMonth }}
+            </n-card>
+        </div>
+        <div class="col">
+            <n-card size="large">
+                Доставлено всего: {{ stats.deliveriedTotal }}
+            </n-card>
+        </div>
+        <div class="col">
+            <n-card size="large">
+                Заработано всего: {{ stats.deliveriedTotal }}
+            </n-card>
+        </div>
     </div>
-    <div class="col">
-      <n-card size="large">
-            Заработано в этом месяце: {{stats.earnedMonth}}
-      </n-card>
-    </div>
-    <div class="col">
-      <n-card size="large">
-            Доставлено всего: {{stats.deliveriedTotal}}
-      </n-card>
-    </div>
-    <div class="col">
-      <n-card size="large">
-            Заработано всего: {{stats.deliveriedTotal}}
-      </n-card>
-    </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
@@ -28,25 +28,24 @@
 import axios from "axios";
 // TODO: authStore получить значение стора из pinia
 const authStore = {
-  id: 1,
+    id: 1,
 }
-const courierId = computed(()=>{
-  return authStore.id
+const courierId = computed(() => {
+    return authStore.id
 })
+
 let stats = ref({
-  data: {
     deliveriedMonth: 24,
     deliveriedTotal: 638,
     earnedMonth: 6846,
     earnedTotal: 23178
-  }
 })
 
-onMounted(async ()=>{
-  const statsRes = await axios.get('https://run.mocky.io/v3/3fbab8a9-f654-4241-b305-73212eddfdf5')
-  if(statsRes) {
-    stats.value = statsRes.data
-  }
+onMounted(async () => {
+    const statsRes = await axios.get('https://run.mocky.io/v3/3fbab8a9-f654-4241-b305-73212eddfdf5')
+    if (statsRes) {
+        stats.value = statsRes.data
+    }
 })
 
 </script>
