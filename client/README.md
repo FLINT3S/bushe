89eb32324e63963926ff8c86c3d2df4656efb50b
Ilya <sia.ilya.dev@gmail.com>
2023-06-24 11:28:14 +0300

    Init client

---
+++ client/README.md
@@ -0,0 +1,5 @@
+#### Шаблон Vue3 + TS + NaiveUI со структурой Feature Sliced Design
+
+#### TODO:
+
+- [ ] Dockerfile для сборки фронтенда и раздачи через Nginx
---
+++ client/vite.config.ts
@@ -0,0 +1,46 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+// @ts-ignore
+import AutoImport from 'unplugin-auto-import/vite'
+// @ts-ignore
+import Components from 'unplugin-vue-components/vite'
+// @ts-ignore
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        AutoImport({
+            imports: [
+                'vue',
+                {
+                    'naive-ui': [
+                        'useDialog',
+                        'useMessage',
+                        'useNotification',
+                        'useLoadingBar'
+                    ],
+                    'vue-router': [
+                        'useRouter',
+                        'useRoute'
+                    ]
+                }
+            ]
+        }),
+        Components({
+            resolvers: [NaiveUiResolver()]
+        })
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, './src'),
+            "@app": path.resolve(__dirname, './src/app'),
+            "@components": path.resolve(__dirname, './src/components'),
+            "@pages": path.resolve(__dirname, './src/components/pages'),
+            "@data": path.resolve(__dirname, './src/data'),
+            "@shared": path.resolve(__dirname, './src/shared'),
+        }
+    }
+})
