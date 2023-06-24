f1b7e0b8ed1771de90891171066de476575bf1d5
tvkoz <tv.kozlov.2002@gmail.com>
2023-06-24 16:09:42 +0300

    Base init database and two ORM

---
+++ backend/data/order.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from sqlmodel import SQLModel, Field, Relationship
+
+from backend.data.orderstatus import OrderStatus
+
+
+class Order(SQLModel, Table=True):
+    __tablename__ = "order"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    status: Optional["OrderStatus"] = Relationship(
+        back_populates="order"
+    )
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+import os
 
-app = FastAPI()
+from starlette.middleware.cors import CORSMiddleware
+
+import uvicorn
+
+from backend.data.databaseservice import DatabaseService
+from backend.services.service import APIService
+
+api = APIService(DatabaseService("postgresql+asyncpg://" + os.environ['POSTGRES_USER'] + ":"
+                                 + os.environ['POSTGRES_PASSWORD'] + "@185.154.193.39:5432/" + os.environ["POSTGRES_DB"]))
+
+api.app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
+@api.app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Bigger Applications!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:api.app", port=3000, reload=True, workers=1)
+    api.database.init_meta_data()
