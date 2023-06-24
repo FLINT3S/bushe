14acd09297bb5de29de3e150f224823a0138f604
tvkoz <tv.kozlov.2002@gmail.com>
2023-06-24 18:17:34 +0300

    Fix error in User ORM

--- backend/data/model/deliverytask.py
+++ backend/data/model/deliverytask.py
@@ -9,7 +9,7 @@ class DeliveryTask(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     user: Optional["User"] = Relationship(
-        back_populates="users"
+        back_populates="delivery_tasks"
     )
     status_id: int = Field(foreign_key="deliverytaskstatus.id")
     status: Optional["TaskStatus"] = Relationship(
