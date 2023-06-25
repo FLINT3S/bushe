04d9e7a6e30d9d8e5ab908ec693065567ce64351
Ilya <sia.ilya.dev@gmail.com>
2023-06-25 16:27:53 +0300

    fix: Кнопка для телеги

--- client/src/components/profile/ProfileStatistics.vue
+++ client/src/components/profile/ProfileStatistics.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="stats__inner d-flex flex-column">
-    <div class="stats-header">статистика</div>
-    <div class="stats__widget">
-      <StatisticWidget :mode="'заработано'" :values="stats.earned"/>
-      <StatisticWidget :mode="'доставлено'" :values="stats.delivered"/>
-      <n-button block class="mb-3" type="primary" secondary>
-          Связь с менеджером
-      </n-button>
-      <OrderHistoryButton to="/completed">история заказов</OrderHistoryButton>
+    <div class="stats__inner d-flex flex-column">
+        <div class="stats-header">статистика</div>
+        <div class="stats__widget">
+            <StatisticWidget :mode="'заработано'" :values="stats.earned"/>
+            <StatisticWidget :mode="'доставлено'" :values="stats.delivered"/>
+            <a class="text-decoration-none" href="https://t.me/zerodaayy" target="_blank">
+                <n-button block class="mb-3" secondary type="primary">
+                    Связь с менеджером
+                </n-button>
+            </a>
+            <OrderHistoryButton to="/completed">история заказов</OrderHistoryButton>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
@@ -17,25 +19,25 @@ import StatisticWidget from "@components/profile/StatisticWidget.vue";
 import OrderHistoryButton from "@shared/components/OrderHistoryButton.vue";
 
 interface Props {
-  stats: {
-    delivered: {
-      perDay: number,
-      perMonth: number
-    },
-    earned: {
-      perDay: number,
-      perMonth: number
-    },
-    income: {
-      monday: number,
-      tuesday: number,
-      wednesday: number,
-      thursday: number,
-      friday: number,
-      saturday: number,
-      sunday: number
+    stats: {
+        delivered: {
+            perDay: number,
+            perMonth: number
+        },
+        earned: {
+            perDay: number,
+            perMonth: number
+        },
+        income: {
+            monday: number,
+            tuesday: number,
+            wednesday: number,
+            thursday: number,
+            friday: number,
+            saturday: number,
+            sunday: number
+        }
     }
-  }
 }
 
 const props = defineProps<Props>()
@@ -43,9 +45,9 @@ const props = defineProps<Props>()
 
 <style scoped>
 .stats-header {
-  font-size: 16px;
-  line-height: 100%;
-  margin-top: 29px;
-  margin-bottom: 15px;
+    font-size: 16px;
+    line-height: 100%;
+    margin-top: 29px;
+    margin-bottom: 15px;
 }
 </style>
